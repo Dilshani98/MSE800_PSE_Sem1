53ea2a7694323f19e3506828f73bb2045468d600
dilshani98 <dilshanisubodha@gmail.com>
2025-12-06 08:49:27 +1300

    week3Activity1

---
+++ Week3/activity1.py
@@ -0,0 +1,34 @@
+class FileRead:
+
+    def readFile(file_path, mode):
+        data = open(file_path,mode, encoding='utf-8')
+
+        with data as file:
+            lines = file.readlines()
+            for line in lines:
+                print(line[0:-1])
+
+        data.close()
+        return lines
+
+    def getCount(lines):
+        count =0 
+        for line in lines:
+            for char in line:
+                if char == '*':
+                    count +=1
+        return count
+        
+
+if __name__ == "__main__":
+
+    filePath = input("Enter file path to read: ")
+    mode = input("Enter mode: ")
+
+    lines = FileRead.readFile(filePath, mode)
+
+    count = FileRead.getCount(lines)
+
+    print (f"Total '*' count in the file: {count}")
+    
+    #C:/YooBee/PSE/Docs/3280709.txt
